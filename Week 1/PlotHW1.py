af81b447b963f34cf945ced8fab4b7602f16514d
kasandraprice <price.kasandra@yahoo.com>
2016-01-21 11:33:54 -0500

    Homework Submission

--- Week 1/PlotHW1.py
+++ Week 1/PlotHW1.py
@@ -1,20 +1,27 @@
-#import numpy and matlibplots module
+import matplotlib.pyplot as plt #import matlibplots 
+plt.rcdefaults()
+import numpy as np   #import numpy
+from numpy import sin,cos,pi # add label cos, sin and pi 
 
+a = np.linspace(0,6*pi,100) #set array from 0 to 6pi 
+y_sin=sin(a)  #calculate y for sine 
+y_cos=cos(a)  #calculate x for cosine 
 
-#generate array of x values from 0 to 6pi
+plt.gca().set_color_cycle(['blue','red']) # estbalish the color for the lines of the graph 
+plt.plot(a,y_sin, '--')  # set plot for  sin 
+plt.plot(a,y_cos)  #set plot for cos
+plt.ylabel("y")    #label y-axis
+plt.xlabel("x")    #label x-axis
 
-#calculate y values for sin wave
+def get_color():   # set color for graph 
+    for item in ['r','b']:
+        yield item
+color= get_color()
 
-#calculate y values for cosine wave
+plt.legend(('sin(x)', 'cos(x)')) #add legend 
+plt.title('Plot of sin(x) and cos(x) from 0 to 6pi')
 
-#plot x and y values for both functions 
-
-#set dashed line for sine wave and solide line for cosine wave
-
-#add legend
-
-
-#leave comment here with your information
-#Name:
-#Date:
-
+plt.show()
+# Kasandra Price
+#@02754360
+#01/21/2016
